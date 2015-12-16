4dc284359b51164f2e962917477e3cc5112ae489
DiegoFernandesAraujo <diego@ccea.uepb.edu.br>
2015-12-16 18:56:24 -0300

    Estrutura de diretórios alterada. Classes desnecessárias deletadas. Faltando conexão_BD, impressão, PDF e tratamento de arquivos TXT.

--- src/com/br/uepb/bsc7/www/main/InventarioUEPB.java
+++ src/com/br/uepb/bsc7/www/main/InventarioUEPB.java
@@ -1,2 +1,12 @@
-package com.br.uepb.bsc7.www;
+package com.br.uepb.bsc7.www.main;
 
+import com.br.uepb.bsc7.www.UI.InventarioUI;
+
+
+public class InventarioUEPB{
+
+    public static void main(String[] args){
+        new InventarioUI().setVisible(true);
+    }
+
+}
